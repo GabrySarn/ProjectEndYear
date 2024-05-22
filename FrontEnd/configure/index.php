c8c17c2177d98f39a20d2d7914291258a33862b6
GabrySarn <gabrics18022005@gmail.com>
2024-05-22 23:08:07 +0200

    as

--- FrontEnd/configure/index.php
+++ FrontEnd/configure/index.php
@@ -475,10 +475,11 @@ include '../../BackEnd/Configure_Back/config.php';
       </div>
     </div>
   </section>
+  
   <div class="u-clearfix u-grey-80 u-footer u-align-center">
     <div class="confirm-button-container">
       <a href="../Riepilogo/riepilogo.html"
-        class="confirm-button u-btn u-btn-rectangle u-button-style u-palette-1-base u-btn-2">Confirm</a>
+        class="u-btn u-btn-rectangle u-button-style u-palette-1-base u-btn-2">Confirm</a>
     </div>
   </div>
 
@@ -489,71 +490,7 @@ include '../../BackEnd/Configure_Back/config.php';
     </div>
   </footer>
 
-  <section
-    class="u-black u-clearfix u-container-style u-dialog-block u-opacity u-opacity-70 u-payment-dialog u-dialog-section-9"
-    id="sec-73c0">
-    <div
-      class="u-align-center u-container-style u-dialog u-hidden-lg u-hidden-sm u-hidden-xl u-hidden-xs u-radius-25 u-shape-round u-white u-dialog-1">
-      <div class="u-container-layout u-valign-top u-container-layout-1">
-        <h5 class="u-align-left u-text u-text-1">Buy Now</h5>
-        <div
-          class="u-expanded-width u-payment-services u-tab-links-align-justify u-tab-payment u-tab-vertical u-tabs u-payment-services-1"
-          data-payment-order-approved-message="Your order has been approved. Thank you!"
-          data-payment-order-cancelled-message="Unable to process your order. Please try again later.">
-          <ul class="u-tab-list u-unstyled" role="tablist">
-            <li class="u-tab-item" role="presentation"><a
-                class="active u-active-grey-10 u-button-style u-payment-paypal u-tab-link u-text-body-color"
-                id="link-tab-9cb3" href="#tab-9cb3" role="tab" aria-controls="tab-9cb3" aria-selected="true"><input
-                  type="radio" class="u-field-input u-grey-15 u-radius-15" name="payment-group"
-                  checked="checked"><span>Paypal</span><svg class="u-svg-content" xmlns="http://www.w3.org/2000/svg"
-                  x="0px" y="0px">
-                  <path fill="#009EE3" d="M11,5.9H18c3.8,0,5.2,1.9,4.9,4.7c-0.3,4.7-3.2,7.2-6.9,7.2h-1.9c-0.5,0-0.8,0.3-0.9,1.3l-0.8,4.3
-  c-0.1,0.3-0.2,0.5-0.5,0.6H7.6c-0.4,0-0.5-0.3-0.4-1l2.6-16C9.9,6.3,10.2,5.9,11,5.9z"></path>
-                  <path fill="#113984" d="M6.7,0h7.1c2,0,4.3,0.1,5.9,1.5c1.1,0.9,1.6,2.4,1.5,4c-0.4,5.4-3.7,8.5-8,8.5H9.6c-0.6,0-0.9,0.4-1.2,1.5
-  l-0.9,5.1C7.4,21,7.3,21.3,7,21.3H2.6C2,21.3,1.9,21,2,20.1L5.2,1.3C5.3,0.4,5.7,0,6.7,0z"></path>
-                  <path fill="#172C70" d="M8.6,14.8l1.3-7.9c0.1-0.7,0.5-1,1.3-1h7.1c1.2,0,2.1,0.2,2.8,0.5c-0.7,4.8-3.8,7.5-7.9,7.5H9.6
-  C9.1,13.9,8.8,14.1,8.6,14.8z"></path>
-                </svg>
-              </a>
-            </li>
-            <li class="u-tab-item" role="presentation"><a
-                class="u-active-grey-10 u-button-style u-payment-stripe u-tab-link u-text-body-color" id="link-tab-ab80"
-                href="#tab-ab80" role="tab" aria-controls="tab-ab80" aria-selected="false"><input type="radio"
-                  class="u-field-input u-grey-15 u-radius-15" name="payment-group"><span>Stripe</span><svg
-                  class="u-svg-content" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 24 24">
-                  <path fill="#635BFF" d="M14,9.2c-2.3-0.8-3.4-1.5-3.4-2.4c0-0.8,0.8-1.3,2-1.3c2.3,0,4.7,0.8,6.4,1.6l0.9-5.5
-  c-1.3-0.5-4-1.6-7.7-1.6C9.6,0,7.5,0.7,5.8,1.9C4.1,3.2,3.4,4.9,3.4,7.2c0,4,2.6,5.7,6.8,7.2c2.7,0.9,3.6,1.6,3.6,2.5
-  c0,0.9-0.9,1.6-2.4,1.6c-1.9,0-5.2-0.9-7.3-2.1L3,22c1.8,0.9,5.1,2,8.7,2c2.8,0,5.1-0.7,6.6-1.9c1.8-1.3,2.7-3.2,2.7-5.7
-  C20.9,12.3,18.2,10.7,14,9.2L14,9.2z"></path>
-                </svg>
-              </a>
-            </li>
-          </ul>
-          <div class="u-tab-content">
-            <div class="u-container-style u-payment-paypal u-tab-active u-tab-pane" id="tab-9cb3" role="tabpanel"
-              aria-labelledby="link-tab-9cb3">
-              <div class="u-container-layout u-payment-services-inner u-container-layout-2">Loading...</div>
-            </div>
-            <div class="u-container-style u-payment-stripe u-tab-pane" id="tab-ab80" role="tabpanel"
-              aria-labelledby="link-tab-ab80">
-              <div class="u-container-layout u-container-layout-3">
-                <a href="#" class="u-btn u-button-style u-stripe-button u-btn-1">Pay with Stripe</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div><button class="u-dialog-close-button u-icon u-text-grey-40 u-icon-1"><svg class="u-svg-link"
-          preserveAspectRatio="xMidYMin slice" viewBox="0 0 16 16" style="">
-          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-efe9"></use>
-        </svg><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
-          xml:space="preserve" class="u-svg-content" viewBox="0 0 16 16" x="0px" y="0px" id="svg-efe9">
-          <rect x="7" y="0" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -3.3138 8.0002)" width="2" height="16">
-          </rect>
-          <rect x="0" y="7" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -3.3138 8.0002)" width="16" height="2">
-          </rect>
-        </svg></button>
-    </div>
-  </section>
+  
   <style>
     .u-dialog-section-9 .u-dialog-1 {
       width: 570px;
@@ -683,25 +620,7 @@ include '../../BackEnd/Configure_Back/config.php';
       background-color: #3572a3;
     }
   </style>
-  <section
-    class="u-align-center u-black u-clearfix u-container-style u-dialog-block u-opacity u-opacity-70 u-dialog-section-10"
-    id="carousel_8bc9">
-    <div
-      class="u-align-center u-border-2 u-border-palette-1-base u-container-style u-dialog u-radius-20 u-shape-round u-white u-dialog-1">
-      <div class="u-container-layout u-container-layout-1">
-        <a href="#" class="u-btn u-btn-round u-button-style u-hover-black u-palette-1-base u-radius-50 u-btn-1">shop
-          now</a>
-      </div><button class="u-dialog-close-button u-icon u-icon-rectangle u-radius-5 u-white u-icon-1"><svg
-          class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 413.348 413.348" style="">
-          <use xlink:href="#svg-cdad"></use>
-        </svg><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
-          xml:space="preserve" class="u-svg-content" viewBox="0 0 413.348 413.348" id="svg-cdad">
-          <path
-            d="m413.348 24.354-24.354-24.354-182.32 182.32-182.32-182.32-24.354 24.354 182.32 182.32-182.32 182.32 24.354 24.354 182.32-182.32 182.32 182.32 24.354-24.354-182.32-182.32z">
-          </path>
-        </svg></button>
-    </div>
-  </section>
+  
   <style>
     .u-dialog-section-10 .u-dialog-1 {
       width: 566px;
