848aaf362923fdf87ef3f0cc8df928fd0740079d
aalericci <alessandro.ricci2005@gmail.com>
2024-05-31 10:08:09 +0200

    a

--- FrontEnd/checkout/index.php
+++ FrontEnd/checkout/index.php
@@ -10,7 +10,7 @@ include '../../BackEnd/Configure_Back/checkout_back.php'
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="">
-  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors"> 
   <meta name="generator" content="Hugo 0.122.0">
   <title>Blazer - Checkout</title>
 
